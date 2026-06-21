0529abd693f6258406938be6fb9e85f1b1e17ec8
angzam78 <angzam78@gmail.com>
2026-06-21 13:58:11 +0200

    Initialize __init__.py with frontend configurations

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# Force ComfyUI to mount the frontend JavaScript directory
+WEB_DIRECTORY = "./js"
+
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+
+__all__ = ["WEB_DIRECTORY", "NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
